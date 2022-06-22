cfb47458e582d27723965e3dda2ee6702f43f86d
jordan <jordanayotte13@outlook.com>
2022-06-21 20:56:58 -0400

    fixed leaderboards to give top 10 of all scores

--- src/Leaderboards.js
+++ src/Leaderboards.js
@@ -49,7 +49,7 @@ function Leaderboards(props) {
         querySnapshot.forEach((doc) => {
             if(highScores.length < 10) {
                 highScores.push({name: doc.id, score: doc.data().score})
-            } if(highScores.length === 10 || querySnapshot.docs.length < 10) {
+            } if(highScores.length === querySnapshot.docs.length) {
                 console.log('yop')
                 highScores.sort((a, b) => a.score - b.score);
                 setScoresLoaded(true)
@@ -67,21 +67,21 @@ function Leaderboards(props) {
                 setHighscorePopUp(<div className='mainBodyLeaderboards'>
                     <h1>Level 1</h1>
                     <button className='backToHome' onClick={() => setBackToHome(true)}>EXIT</button>
-                    {highScores.map((obj) => <div style={highScores.indexOf(obj) !== 0 ? {width: '250px', fontSize: '22px'} : {width: '250px', color: 'green', fontSize: '30px'}}>{`${highScores.indexOf(obj) + 1}. ${obj.name}: ${obj.score} seconds`}</div>)}
+                    {highScores.map((obj) => highScores.indexOf(obj) < 10 ? <div style={highScores.indexOf(obj) !== 0 ? {width: '250px', fontSize: '22px'} : {width: '250px', color: 'green', fontSize: '30px'}}>{`${highScores.indexOf(obj) + 1}. ${obj.name}: ${obj.score} seconds`}</div> : null)}
                 </div>)
             }
             if(level === 2) {
                 setHighscorePopUp(<div className='mainBodyLeaderboards'>
                     <h1>Level 2</h1>
                     <button className='backToHome' onClick={() => setBackToHome(true)}>EXIT</button>
-                    {highScores.map((obj) => <div style={highScores.indexOf(obj) !== 0 ? {width: '250px', fontSize: '22px'} : {width: '250px', color: 'green', fontSize: '30px'}}>{`${highScores.indexOf(obj) + 1}. ${obj.name}: ${obj.score} seconds`}</div>)}
+                    {highScores.map((obj) => highScores.indexOf(obj) < 10 ? <div style={highScores.indexOf(obj) !== 0 ? {width: '250px', fontSize: '22px'} : {width: '250px', color: 'green', fontSize: '30px'}}>{`${highScores.indexOf(obj) + 1}. ${obj.name}: ${obj.score} seconds`}</div> : null)}
                 </div>)
             }
             if(level === 3) {
                 setHighscorePopUp(<div className='mainBodyLeaderboards'>
                     <h1>Level 3</h1>
                     <button className='backToHome' onClick={() => setBackToHome(true)}>EXIT</button>
-                    {highScores.map((obj) => <div style={highScores.indexOf(obj) !== 0 ? {width: '250px', fontSize: '22px'} : {width: '250px', color: 'green', fontSize: '30px'}}>{`${highScores.indexOf(obj) + 1}. ${obj.name}: ${obj.score} seconds`}</div>)}
+                    {highScores.map((obj) => highScores.indexOf(obj) < 10 ? <div style={highScores.indexOf(obj) !== 0 ? {width: '250px', fontSize: '22px'} : {width: '250px', color: 'green', fontSize: '30px'}}>{`${highScores.indexOf(obj) + 1}. ${obj.name}: ${obj.score} seconds`}</div> : null)}
                 </div>)
             }
     },[level, scoresLoaded])
